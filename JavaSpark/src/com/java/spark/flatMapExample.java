2b0d47470a1b6cce3bb86464079c7de59984027d
Abhishek Singh <abhisheksingh2009iu@gmail.com>
2017-12-13 01:35:41 +0800

    Added code for FlatMap

---
+++ JavaSpark/src/com/java/spark/flatMapExample.java
@@ -0,0 +1,30 @@
+package com.java.spark;
+
+import java.util.Arrays;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+public class flatMapExample {
+
+public static void main(String args[]) {
+		
+		String master,inputpath ;
+		
+	    if (args.length < 2) {
+	      System.out.println("Insufficient parameter.");
+	      System.exit(1);
+	    } else {
+	      master = args[0];
+	      inputpath = args[1];
+	    
+	    SparkConf conf = new SparkConf().setAppName("mapExample").setMaster(master);
+		JavaSparkContext sc = new JavaSparkContext(conf);
+		JavaRDD<String> rdd = sc.textFile(inputpath);
+		JavaRDD<String> mapdata = rdd.flatMap(f -> Arrays.asList(f.split(" ")).iterator());
+		mapdata.collect().forEach( x -> System.out.println(x));
+	    }
+	}
+
+}
