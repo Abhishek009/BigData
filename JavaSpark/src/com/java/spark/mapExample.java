6c4ee0342738893662b32e9a274b2c5e20d85a45
Abhishek Singh <abhisheksingh2009iu@gmail.com>
2017-12-14 21:48:03 +0800

    Modifed flatmap and Map example

--- JavaSpark/src/com/java/spark/mapExample.java
+++ JavaSpark/src/com/java/spark/mapExample.java
@@ -1,7 +1,5 @@
 package com.java.spark;
 
-import java.util.Arrays;
-
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
@@ -9,21 +7,19 @@ import org.apache.spark.api.java.JavaSparkContext;
 public class mapExample {
 
 	public static void main(String args[]) {
-		
-		String master,inputpath ;
-		
-	    if (args.length < 2) {
-	      System.out.println("Insufficient parameter.");
-	      System.exit(1);
-	    } else {
-	      master = args[0];
-	      inputpath = args[1];
-	    
-	    SparkConf conf = new SparkConf().setAppName("mapExample").setMaster(master);
-		JavaSparkContext sc = new JavaSparkContext(conf);
-		JavaRDD<String> rdd = sc.textFile(inputpath);
-		JavaRDD<Integer> mapdata = rdd.map(f -> Integer.parseInt(f)*Integer.parseInt(f));
-		mapdata.collect().forEach( x -> System.out.println(x));
-	    }
+
+		String master, inputpath;
+		if (args.length < 2) {
+			System.out.println("Insufficient parameter.");
+			System.exit(1);
+		} else {
+			master = args[0];
+			inputpath = args[1];
+			SparkConf conf = new SparkConf().setAppName("mapExample").setMaster(master);
+			JavaSparkContext sc = new JavaSparkContext(conf);
+			JavaRDD<String> rdd = sc.textFile(inputpath);
+			JavaRDD<Integer> mapdata = rdd.map(f -> Integer.parseInt(f) * Integer.parseInt(f));
+			mapdata.collect().forEach(x -> System.out.println(x));
+		}
 	}
 }
